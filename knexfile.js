b692eff3377d5623d847aea70072d8c296e0e31d
Gustav <gustav@onsalait.se>
2019-05-30 18:47:02 +0200

    First commit

---
+++ knexfile.js
@@ -0,0 +1,25 @@
+
+module.exports = {
+    client: "mysql",
+    connection: {
+        user: "root",
+        password: "password",
+        database: "tutorial_node_database"
+    }
+}
+
+
+
+
+/*
+module.exports = {
+    client: 'mysql',
+    connection: {
+      user: 'root',
+      password: 'password',
+      database: 'tutorial_node_database'
+    }
+  }
+
+*/ 
+
